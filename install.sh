e7f55d048df34e9f3794770de3a7505ec7a57843
Aiden <88350376+intothebeans@users.noreply.github.com>
2025-07-06 21:48:09 +0100

    🔨 script(stow): fix the stow command
    Signed-off-by: Aiden <88350376+intothebeans@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ case $choice in
 y | Y)
     ./dependencies.sh
     ./vim.sh
-    stow -t $HOME -d ../dots
+    stow -t $HOME -d ../dots .
     echo "export EDITOR=vim" >>$HOME/.zsh_env
     echo "export SUDO_EDITOR=vim" >>$HOME/.zsh_env
     echo "export PATH=$PATH:$HOME/.local/bin" >>$HOME/.zsh_env
@@ -18,7 +18,7 @@ n | N)
     echo "export EDITOR=neovim" >>$HOME/.zsh_env
     echo "export SUDO_EDITOR=$HOME/.local/share/bob/nvim-bin/nvim" >>$HOME/.zsh_env
     echo "export PATH=$PATH:$HOME/.local/bin:$HOME/.local/share/bob/nvim-bin" >>$HOME/.zsh_env
-    stow -t $HOME -d ../dots
+    stow -t $HOME -d ../dots .
     ;;
 *) echo "Invalid choice" ;;
 esac
