7450df3418976ccec47c6fc2fea9572dc6de644a
Aiden <88350376+intothebeans@users.noreply.github.com>
2025-07-07 16:44:11 -0400

    🩹 fix-simple: fix read throwing flag error
    Signed-off-by: Aiden <88350376+intothebeans@users.noreply.github.com>

--- dependencies.sh
+++ dependencies.sh
@@ -6,7 +6,7 @@ echo "Welcome to the Beans Server Dotfiles installer!"
 echo "This script will install all necessary dependencies."
 echo "============================================="
 
-read -p "Install nala? " REPLY -r
+read -p "Install nala? " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     echo "Installing nala"
     curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
@@ -15,32 +15,32 @@ fi
 
 # main dependencies
 sudo nala update
-sudo nala install git curl wget zsh tmux ripgrep bat unzip trash-cli build-essential python3-pip gpg rsync less stow cmake gcc
+sudo nala install git curl wget zsh tmux ripgrep bat unzip trash-cli build-essential python3-pip gpg rsync less stow
 # fix batcat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
 # install tpm
-read -p "Do you want to install tpm? ([y]/n) " REPLY -r
+read -p "Do you want to install tpm? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # install fzf
-read -p "Do you want to install fzf? ([y]/n) " REPLY -r
+read -p "Do you want to install fzf? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 fi
 
 # install zoxide
-read -p "Do you want to install zoxide? ([y]/n) " REPLY -r
+read -p "Do you want to install zoxide? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 fi
 
 # install eza
-read -p "Do you want to install eza? ([y]/n) " REPLY -r
+read -p "Do you want to install eza? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     sudo mkdir -p /etc/apt/keyrings
     wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
@@ -51,7 +51,7 @@ if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
 fi
 
 # install starship
-read -p "Do you want to install starship? ([y]/n) " REPLY -r
+read -p "Do you want to install starship? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     curl -sS https://starship.rs/install.sh | sh
     starship preset bracketed-segments -o ~/.config/starship.toml
@@ -59,7 +59,7 @@ fi
 
 # install commitizen
 echo "Warning: commitizen installs using pip with --break-system-packages, this may break your system."
-read -p "Do you want to install commitizen? ([y]/n) " REPLY -r
+read -p "Do you want to install commitizen? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     python3 -m pip install commitizen cz-conventional-gitmoji --user --break-system-packages
 fi
--- nvim.sh
+++ nvim.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # install neovim
-# can also be used to update neovim
-read -p "Do you want to install neovim? ([y]/n) " REPLY -r
+read -p "Do you want to install neovim? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     ARCH=$(uname -m)
     wget https://github.com/neovim/neovim/releases/download/latest/nvim-linux-$ARCH.tar.gz -O nvim.tar.gz
@@ -13,7 +12,7 @@ if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
 fi
 
 # install nvm for plugins
-read -p "Do you want to install nvm? ([y]/n) " REPLY -r
+read -p "Do you want to install nvm? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 fi
