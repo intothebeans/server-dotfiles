5ad79e0404d20442ea7266a065dced757e934263
Aiden <88350376+intothebeans@users.noreply.github.com>
2025-07-07 17:09:42 -0400

    🔨 script(nvim.sh): add update option and fix url
    Signed-off-by: Aiden <88350376+intothebeans@users.noreply.github.com>

--- nvim.sh
+++ nvim.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+install_nvim() {
+    ARCH=$(uname -m)
+    wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-$ARCH.tar.gz -O nvim.tar.gz
+    tar -xvf nvim.tar.gz
+    mv ./nvim-linux-$ARCH/ $HOME/.local/nvim
+    rm nvim.tar.gz
+    ln -s $HOME/.local/nvim/bin/nvim $HOME/.local/bin/nvim
+}
+
 # install neovim
 read -p "Do you want to install neovim? ([y]/n) " REPLY
 if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
-    ARCH=$(uname -m)
-    wget https://github.com/neovim/neovim/releases/download/latest/nvim-linux-$ARCH.tar.gz -O nvim.tar.gz
-    tar -xvf nvim.tar.gz
-    mv ./nvim/ $HOME/.local/
-    rm nvim.tar.gz
-    ln -s $HOME/.local/nvim/bin/nvim $HOME/.local/bin/nvim
+    install_nvim
+fi
+
+read -p "Do you want to update neovim? ([y]/n) " REPLY
+if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
+    if [[ $INSTALL_DIR ]]; then
+        tar -czf $HOME/.local/nvim-backup-$(date +"%Y%m%d%H%M%S") $INSTALL_DIR
+        rm -rf $INSTALL_DIR
+    fi
+    install_nvim
 fi
 
 # install nvm for plugins
